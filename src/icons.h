d8befa6f584766b736ef175e88d754d38fac19e5
Rick Gleason <fcgleason@tgp-architects.com>
2020-08-04 21:57:07 -0400

    v1.2.4.0 + fe2 v1.0.132 + icons

--- src/icons.h
+++ src/icons.h
@@ -3,12 +3,18 @@
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
-#include <wx/wx.h>
+#     include <wx/wx.h>
 #endif
 
 extern void initialize_images(void);
 
 extern wxBitmap *_img_findit_pi;
 extern wxBitmap *_img_findit;
+
+#ifdef PLUGIN_USE_SVG
+extern wxString _svg_findit;
+extern wxString _svg_findit_rollover;
+extern wxString _findit_toggled;
+#endif
 
 #endif /* ICONS_H */
