880e0dcd75a476b6b7903bed2345a3c0212c1f68
Rick Gleason <fcgleason@tgp-architects.com>
2021-12-10 21:49:59 -0500

    1.2.13.0 + TP 1.0.185.0 fix macos

--- include/gui.h
+++ include/gui.h
@@ -1,18 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jun 30 2011)
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
 #ifndef __GUI_H__
 #define __GUI_H__
 
+#pragma once
+
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
 #include <wx/intl.h>
-#include <wx/string.h>
 #include <wx/button.h>
+#include <wx/string.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
 #include <wx/gdicmn.h>
 #include <wx/font.h>
 #include <wx/colour.h>
@@ -22,9 +27,6 @@
 #include <wx/sizer.h>
 #include <wx/grid.h>
 #include <wx/panel.h>
-#include <wx/bitmap.h>
-#include <wx/image.h>
-#include <wx/icon.h>
 #include <wx/notebook.h>
 #include <wx/dialog.h>
 #include <wx/checkbox.h>
@@ -39,189 +41,97 @@
 ///////////////////////////////////////////////////////////////////////////////
 class FindItDialog : public wxDialog
 {
-private:
+	private:
 
-protected:
-    wxNotebook* m_notebook1;
-    wxPanel* m_panel1;
-    wxButton* m_buttonaddLineMaterial;
-    wxStaticText* m_staticText1;
-    wxTextCtrl* m_textCtrl1;
-    wxPanel* m_panel2;
-    wxButton* m_buttonaddLineFood;
-    wxStaticText* m_staticText2;
-    wxTextCtrl* m_textCtrl11;
-    wxPanel* m_panel3;
-    wxButton* m_buttonaddLineLocations;
-    wxPanel* m_panel4;
-    wxButton* m_buttonaddLineUnits;
-    wxStdDialogButtonSizer* m_sdbSizer;
-    wxButton* m_sdbSizerOK;
-    wxButton* m_sdbSizerCancel;
+	protected:
+		wxNotebook* m_notebook1;
+		wxPanel* m_panel1;
+		wxButton* m_buttonaddLineMaterial;
+		wxStaticText* m_staticText1;
+		wxTextCtrl* m_textCtrl1;
+		wxPanel* m_panel2;
+		wxButton* m_buttonaddLineFood;
+		wxStaticText* m_staticText2;
+		wxTextCtrl* m_textCtrl11;
+		wxPanel* m_panel3;
+		wxButton* m_buttonaddLineLocations;
+		wxPanel* m_panel4;
+		wxButton* m_buttonaddLineUnits;
+		wxStdDialogButtonSizer* m_sdbSizer;
+		wxButton* m_sdbSizerOK;
+		wxButton* m_sdbSizerCancel;
 
-    // Virtual event handlers, overide them in your derived class
-    virtual void OnCloseDialog( wxCloseEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnInit( wxInitDialogEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnButtonClickMaterialAdd( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnTextMaterial( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnButtonClickBuyMaterial( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnCharMaterial( wxKeyEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellChangeMaterial( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellLeftClickMaterial( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridLabelLClickMaterial( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnButtonClickAddLineFood( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnTextFood( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnButtonClickBuyFood( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnCharFood( wxKeyEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellChangeFood( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellLeftClickFood( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridLabelLClickFood( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void onButtonClickAddLineLocations( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnCharLocations( wxKeyEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellChangeLocations( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellLeftClickLocations( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void onEditorShownLocations( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnButtonClickUnitsAddLine( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnCharUnits( wxKeyEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellChangeUnits( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridCellLeftClickUnits( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void onEditorShownUnits( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnCancelClick( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnOKClick( wxCommandEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridSelectCellMaterial( wxGridEvent& event )
-    {
-        event.Skip();
-    }
-    virtual void OnGridSelectCellFood( wxGridEvent& event )
-    {
-        event.Skip();
-    }
+		// Virtual event handlers, override them in your derived class
+		virtual void OnCloseDialog( wxCloseEvent& event ) { event.Skip(); }
+		virtual void OnInit( wxInitDialogEvent& event ) { event.Skip(); }
+		virtual void OnButtonClickMaterialAdd( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnTextMaterial( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnButtonClickBuyMaterial( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnCharMaterial( wxKeyEvent& event ) { event.Skip(); }
+		virtual void OnGridCellChangeMaterial( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridCellLeftClickMaterial( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridLabelLClickMaterial( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnButtonClickAddLineFood( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnTextFood( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnButtonClickBuyFood( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnCharFood( wxKeyEvent& event ) { event.Skip(); }
+		virtual void OnGridCellChangeFood( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridCellLeftClickFood( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridLabelLClickFood( wxGridEvent& event ) { event.Skip(); }
+		virtual void onButtonClickAddLineLocations( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnCharLocations( wxKeyEvent& event ) { event.Skip(); }
+		virtual void OnGridCellChangeLocations( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridCellLeftClickLocations( wxGridEvent& event ) { event.Skip(); }
+		virtual void onEditorShownLocations( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnButtonClickUnitsAddLine( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnCharUnits( wxKeyEvent& event ) { event.Skip(); }
+		virtual void OnGridCellChangeUnits( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnGridCellLeftClickUnits( wxGridEvent& event ) { event.Skip(); }
+		virtual void onEditorShownUnits( wxGridEvent& event ) { event.Skip(); }
+		virtual void OnCancelClick( wxCommandEvent& event ) { event.Skip(); }
+		virtual void OnOKClick( wxCommandEvent& event ) { event.Skip(); }
 
 
-public:
-    wxButton* m_buttonBuyItMaterial;
-    wxGrid* m_gridMaterial;
-    wxButton* m_buttonBuyItFood;
-    wxGrid* m_gridFood;
-    wxGrid* m_gridLocations;
-    wxGrid* m_gridUnits;
+	public:
+		wxButton* m_buttonBuyItMaterial;
+		wxGrid* m_gridMaterial;
+		wxButton* m_buttonBuyItFood;
+		wxGrid* m_gridFood;
+		wxGrid* m_gridLocations;
+		wxGrid* m_gridUnits;
 
-    FindItDialog( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("FindIt"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 943,518 ), long style = wxCLOSE_BOX|wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER|wxSTAY_ON_TOP);
-    ~FindItDialog();
+		FindItDialog( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("FindIt"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 943,518 ), long style = wxCLOSE_BOX|wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
+
+		~FindItDialog();
 
 };
 
-class findit_pi;
 ///////////////////////////////////////////////////////////////////////////////
 /// Class OptionsDialog
 ///////////////////////////////////////////////////////////////////////////////
 class OptionsDialog : public wxDialog
 {
-private:
-    findit_pi* parent;
-protected:
-    wxStaticLine* m_staticline1;
-    wxStdDialogButtonSizer* m_sdbSizer2;
-    wxButton* m_sdbSizer2OK;
-    wxButton* m_sdbSizer2Cancel;
+	private:
 
-    // Virtual event handlers, overide them in your derived class
-    virtual void OnInitDialog( wxInitDialogEvent& event ) ;
+	protected:
+		wxStaticLine* m_staticline1;
+		wxStdDialogButtonSizer* m_sdbSizer2;
+		wxButton* m_sdbSizer2OK;
+		wxButton* m_sdbSizer2Cancel;
+
+		// Virtual event handlers, override them in your derived class
+		virtual void OnInitDialog( wxInitDialogEvent& event ) { event.Skip(); }
 
 
-public:
-    wxCheckBox* m_checkBoxFindItIcon;
-    wxRadioBox* m_radioBox1;
-    wxRadioBox* m_radioBox11;
-    wxRadioBox* m_radioBox5;
+	public:
+		wxCheckBox* m_checkBoxFindItIcon;
+		wxRadioBox* m_radioBox1;
+		wxRadioBox* m_radioBox11;
+		wxRadioBox* m_radioBox5;
 
-    OptionsDialog( wxWindow* parent, findit_pi* p, wxWindowID id = wxID_ANY, const wxString& title = _("FindIt Options"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 257,257 ), long style = wxDEFAULT_DIALOG_STYLE );
-    ~OptionsDialog();
+		OptionsDialog( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("FindIt Options"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 257,257 ), long style = wxDEFAULT_DIALOG_STYLE );
+
+		~OptionsDialog();
 
 };
 
-#endif //__GUI_H__
